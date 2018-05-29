5f690651304fe63c04ef11a26c66932fccf75c4f
toygansevim <toygansevim@icloud.com>
2018-05-28 19:38:29 -0700

    Initial commit

---
+++ src/baconMeter/Graph.java
@@ -0,0 +1,5 @@
+package baconMeter;
+
+public class Graph
+{
+}
---
+++ src/baconMeter/TestBacon.java
@@ -0,0 +1,23 @@
+package baconMeter;
+
+public class TestBacon
+{
+
+    public static void main(String[] args)
+    {
+        //This is the console that will start the program
+    }
+
+
+    public static void displayMenu(int choice)
+    {
+
+        System.out.println("Welcome to the Six-Degrees of Seperation ");
+        switch (choice){
+
+
+            case
+        }
+    }
+
+}
